db7e30838cd563239713db2269b9b9d19fbe0f0b
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-10-10 13:14:17 +0700

    update executor service

---
+++ demo-java-thread/src/test/java/com/rizkiaazmi/demojavathread/ExecutorServiceTest.java
@@ -0,0 +1,46 @@
+package com.rizkiaazmi.demojavathread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ExecutorServiceTest {
+    @Test
+    void testExecutorService() throws InterruptedException {
+
+        var executor = Executors.newSingleThreadExecutor();
+
+        for (int i = 0; i < 100; i++) {
+            executor.execute(() -> {
+                try {
+                    Thread.sleep(200);
+                    System.out.println("Runnable in Thread : " + Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+
+    @Test
+    void testExecutorServiceFix() throws InterruptedException {
+
+        var executor = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 100; i++) {
+            executor.execute(() -> {
+                try {
+                    Thread.sleep(200);
+                    System.out.println("Runnable in Thread : " + Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
