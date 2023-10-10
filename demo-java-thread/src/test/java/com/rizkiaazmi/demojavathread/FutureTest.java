7df6d24bfe8bbf84c7981e1cb96d14d05e2ecd1f
Rizki Abdillah Azmi <rizkiabdillahazmi10@gmail.com>
2023-10-10 13:40:34 +0700

    update callable and future

---
+++ demo-java-thread/src/test/java/com/rizkiaazmi/demojavathread/FutureTest.java
@@ -0,0 +1,33 @@
+package com.rizkiaazmi.demojavathread;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureTest {
+
+    @Test
+    void futureGet() throws ExecutionException, InterruptedException {
+
+        var executor = Executors.newVirtualThreadPerTaskExecutor();
+
+        Callable<String> callable = () -> {
+            Thread.sleep(5_000);
+            return "Hi";
+        };
+
+        Future<String> future = executor.submit(callable);
+        System.out.println("future completed");
+
+        while (!future.isDone()) {
+            System.out.println("waiting future");
+            Thread.sleep(1_000);
+        }
+
+        String value = future.get();
+        System.out.println(value);
+    }
+}
